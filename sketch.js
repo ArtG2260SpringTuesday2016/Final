98712506783e8471430caca2958a089992913f25
AnthonyBrandao <Brandao.a@husky.neu.edu>
2016-04-19 13:20:31 -0400

    new test animation

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+var number;
+
+function preload(){
+
+number = loadAnimation('assets/Number00.png','assets/Number07.png');
+
+}
 function setup() {
-  
+  createCanvas(600,600);
 }
 
 function draw() {
-  
+  background(250,250,250);
+  animation(number,200,200);
 }
