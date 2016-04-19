15af20326244d2ecfc9b391edd5945281e7685e6
jillbook <jillbook@Jills-MacBook-Pro.local>
2016-04-19 17:50:59 -0400

    gathering data
    started gathering data for infographic and proposed project idea

---
+++ Final.js/data.js
@@ -0,0 +1,201 @@
+var pizzaData = [
+  {"State":"Alabama",
+   "PizzaHuts":"103",
+   "Dominos":"153"},
+   
+  {"State":"Alaska",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Arizona",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Arkansas",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"California",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Colorado",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Connecticut",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Delaware",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Florida",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Georgia",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Hawaii",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Idaho",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Illinios",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Indiana",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Iowa",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Kansas",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Kentucky",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Louisiana",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Maine",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Maryland",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Massachusetts",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Michigan",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Minnesota",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Mississippi",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Missouri",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Montana",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Nebraska",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Nevada",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"New Hampshire",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"New Jersey",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"New Mexico",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"New York",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"North Carolina",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"North Dakota",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Ohio",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Oklahoma",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Oregon",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Pennsylvania",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Rhode Island",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"South Carolina",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"South Dakota",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Tennessee",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Texas",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Utah",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Vermont",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Virginia",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Washington",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"West Virginia",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Wisconsin",
+   "PizzaHuts":"",
+   "Dominos":""},
+   
+  {"State":"Wyoming",
+   "PizzaHuts":"",
+   "Dominos":""}
+  ]
