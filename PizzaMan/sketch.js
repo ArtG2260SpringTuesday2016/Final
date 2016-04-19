637df1162aa2989cfefa5082b5b044b9c4b96e87
shelrobin <shelbyrobin7@gmail.com>
2016-04-19 19:45:38 -0400

    not working
    trying to make it when you press a key it lets you then put on
    pepperoni

--- PizzaMan/sketch.js
+++ PizzaMan/sketch.js
@@ -1,36 +1,47 @@
+var buttonP;
+var pepperonis = [];
+var xPos = 250;
+var yPos = 200;
+var lilR = 200;
+var bigR = 250
 
-  
 this.distanceToMouse= function(){
   return dist(mouseX, mouseY, 150, 200);
 }
-var pepperonis = [];
 
 function setup() {
   createCanvas(500,500)
    background("white");
+  
   for (var i=0; i < 100; i++)
   pepperonis[i] = new Pepperoni();
-}
 
-function mousePressed(){
-  if (dist(mouseX,mouseY,150,200)< 90){
-  pepperonis.push(new Pepperoni(mouseX,mouseY));
-}
 }
 
 function draw() {
  
   noStroke();
   fill("#dba24a");
-  ellipse(150,200,250,250);
+  ellipse(xPos,yPos,bigR,bigR);
   fill("#FFD700");
-  ellipse(150,200,200,200);
+  ellipse(xPos,yPos,lilR,lilR);
+
+if (mouseIsPressed) {
+  if (mouseButton == LEFT)
+      if (mouseIsPressed && (dist(mouseX,mouseY,xPos,yPos)< (lilR/2)-10)){
+      pepperonis.push(new Pepperoni(mouseX,mouseY));
+      }
+    if (mouseButton == RIGHT)
+    fill('black')
+      rect(10,10,10,10);
+}
+}
 
 
  for (var i=0; i < pepperonis.length; i++){
  pepperonis[i].display();
  }
-}
+
 
 function Pepperoni(x,y){
   this.x = x;
@@ -42,7 +53,3 @@ function Pepperoni(x,y){
     ellipse(this.x,this.y, 20,20)
   }
 }
-
-
-
-
