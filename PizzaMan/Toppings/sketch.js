237d37ea9e433cd32e60abea9e6574bc5ea24a90
shelrobin <shelbyrobin7@gmail.com>
2016-04-20 15:39:06 -0400

    Stuck
    trying to make pizza an object, and check if mouse is inside it

--- PizzaMan/Toppings/sketch.js
+++ PizzaMan/Toppings/sketch.js
@@ -1,9 +1,14 @@
 var toppings = [];
 var CurrentTopping;
+var WholePizza;
 
 function setup(){
   createCanvas(500,500);
-  CurrentTopping = new Toppings();
+  CurrentTopping = new Topping();
+  WholePizza = new Pizza();
+  ToppingX = mouseX;
+  ToppingY = mouseY;
+
 }
 
 function draw(){
@@ -23,32 +28,56 @@ function draw(){
   ellipse(55,49,26,13)
   rect(49,50,12,15)
   
+  WholePizza.renderPizza(150,200);
   CurrentTopping.renderPepperoni();
   CurrentTopping.renderMushroom();
   CurrentTopping.renderPineapple();
+
 }
 
-function Toppings(x,y){
-  this.x = 10;
-  this.y = 10;
   
-this.renderPepperoni = function(x,y){  //pepperoni
+function Topping(ToppingX,ToppingY){
+  this.x = ToppingX;
+  this.y = ToppingY;
+  
+this.renderPepperoni = function(ToppingX,ToppingY){  //pepperoni
       noStroke();
       fill("#E34234");
-      ellipse(x,y,20,20)
+      ellipse(ToppingX,ToppingY,20,20)
   }
 
-this.renderMushroom = function(x,y){ //mushroom
+this.renderMushroom = function(ToppingX, ToppingY){ //mushroom
     fill("#E9C2A6");
-    ellipse(x+1,y+1,15,15)
-    ellipse(x+11,y+1,15,15)
-    ellipse(x+6,y,26,13)
-    rect(x,y+1,12,15)
+    ellipse(ToppingX+1,ToppingY+1,15,15)
+    ellipse(ToppingX+11,ToppingY+1,15,15)
+    ellipse(ToppingX+6,ToppingY,26,13)
+    rect(ToppingX,ToppingY+1,12,15)
   }
 
-this.renderPineapple= function(x,y){ //pineapple
+this.renderPineapple= function(ToppingX,ToppingY){ //pineapple
       fill('#ECDC98')
       noStroke();
-      triangle(x+20, y ,x, y+10, x+20, y+20);
+      triangle(ToppingX+20, ToppingY ,ToppingX, ToppingY+10, ToppingX+20, ToppingY+20);
   }    
 }
+
+function Pizza(PizzaX,PizzaY){
+  this.x = PizzaX;
+  this.y = PizzaY;
+
+  this.renderPizza= function(PizzaX,PizzaY){
+  noStroke();
+  fill("#dba24a");
+  ellipse(PizzaX,PizzaY,250,250);
+  fill("#FFD700");
+  ellipse(PizzaX,PizzaY,200,200);
+  
+  
+function mousePressed(){
+ if (dist(ToppingX,ToppingY,PizzaX, PizzaY)< 90){
+  toppings.push(new Topping(ToppingX,ToppingY));
+    }
+  }
+}
+}
+//&& dist(ToppingX,ToppingY,PizzaX, PizzaY)< 90 = true
