6b6b8bb78559e0283fff46a16531ee03c7d35e09
Alexis Diefenbach <aodiefenbach@gmail.com>
2016-04-19 20:01:54 -0400

    Added files via upload

--- sketch.js
+++ sketch.js
@@ -1,46 +1,23 @@
-var block1;
-var block2;
-var block3;
-var block4;
-var block5;
-var block6;
-var block7;
-var block8;
-var block9;
-var block10;
-var block11;
-var block12;
-var block13;
-var block14;
-var block15;
-var player;
-var coins;
+var blocks = [];
 
 function setup() {
   createCanvas(550, 400);
+  var blockade = [230,50, 25, 300,50, 50, 200, 25,160, 87, 25, 100,98, 125, 100, 25,90, 50, 180, 25,400, 50, 200, 25,300, 87, 25, 100,402, 125, 180, 25,352, 50, 270, 25,352, 50, 270, 25,260, 270, 420, 25,480, 242, 25, 210,63, 305, 25, 60,185, 340, 270, 25,440, 340, 100, 25];
   coins = new Group();
-  block1 = createSprite(230, 50, 25, 300);
-  block2 = createSprite(50, 50, 200, 25);
-  block3 = createSprite(160, 87, 25, 100);
-  block4 = createSprite(98, 125, 100, 25);
-  block5 = createSprite(90, height/2, 180, 25);
-  block6 = createSprite(400, 50, 200, 25);
-  block7 = createSprite(300, 87, 25, 100);
-  block8 = createSprite(402, 125, 180, 25)
-  block9 = createSprite(352, height/2, 270, 25);
-  block10 = createSprite(352, height/2, 270, 25);
-  block11 = createSprite(260, 270, 420, 25);
-  block12 = createSprite(480, 242, 25, 210);
-  block13 = createSprite(63, 305, 25, 60);
-  block14 = createSprite(185, 340, 270, 25);
-  block15 = createSprite(440, 340, 100, 25);
+  var bc = function() {
+    for (var i = 0; i < 60; i+=4) {
+    blocks.push(createSprite(blockade[i],blockade[i + 1],blockade[i + 2],blockade[i + 3]));
+    } 
+  }
+  bc();
+  console.log(blocks);
   player = createSprite(0, 0, 25, 25);
   player.shapeColor = color('red');
-  var c = createSprite(120,90,20, 20);
-  var d = createSprite(100,305,20, 20);
-  var e = createSprite(340,90,20, 20);
-  var f = createSprite(440,165,20, 20);
-  var g = createSprite(440,235,20, 20);
+  var c = createSprite(120, 90, 20, 20);
+  var d = createSprite(100, 305, 20, 20);
+  var e = createSprite(340, 90, 20, 20);
+  var f = createSprite(440, 165, 20, 20);
+  var g = createSprite(440, 235, 20, 20);
   c.shapeColor = color(255, 255, 0);
   d.shapeColor = color(255, 255, 0);
   e.shapeColor = color(255, 255, 0);
@@ -51,35 +28,28 @@ function setup() {
   coins.add(e);
   coins.add(f);
   coins.add(g);
-  }
-  
+}
+
 function draw() {
+  var blockade = [230, 50, 25, 300,50, 50, 200, 25,160, 87, 25, 100,98, 125, 100, 25,90, 50, 180, 25,400, 50, 200, 25,300, 87, 25, 100,402, 125, 180, 25,352, 50, 270, 25,352, 50, 270, 25,260, 270, 420, 25,480, 242, 25, 210,63, 305, 25, 60,185, 340, 270, 25,440, 340, 100, 25];
   background(150);
-  player.velocity.x = (mouseX-player.position.x)*0.2;
-  player.velocity.y = (mouseY-player.position.y)*0.2;
+  player.velocity.x = (mouseX - player.position.x) * 0.2;
+  player.velocity.y = (mouseY - player.position.y) * 0.2;
   player.overlap(coins, getCoin);
-  player.collide(block1);
-  player.collide(block2);
-  player.collide(block3);
-  player.collide(block4);
-  player.collide(block5);
-  player.collide(block6);
-  player.collide(block7);
-  player.collide(block8);
-  player.collide(block9);
-  player.collide(block10);
-  player.collide(block11);
-  player.collide(block12);
-  player.collide(block13);
-  player.collide(block14);
-  player.collide(block15);
+  var bc = function() {
+    for (var i = 0; i < 15; i++) {
+      player.collide(blocks[i]);
+    }
+  }
+
+  bc();
   drawSprites();
   if (coins.length == 0) {
-   fill(150);
-   rect(0,0,600,400);
-   textSize(50);
-   fill('red');
-   text("you win!", 170, 200);
+    fill(150);
+    rect(0, 0, 600, 400);
+    textSize(50);
+    fill('red');
+    text("you win!", 170, 200);
   }
 }
 
