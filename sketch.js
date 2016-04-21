281e9c4ab3ed94ef6249005e786f3291e6eac8ef
AnthonyBrandao <Brandao.a@husky.neu.edu>
2016-04-20 23:27:54 -0400

    new additions

--- sketch.js
+++ sketch.js
@@ -1,16 +1,54 @@
 var number;
-
+var spr;
+var wiggle;
+var rainbow;
 function preload(){
+
+rainbow = loadAnimation("assets/Circle01.png","assets/Circle05.png")
+
 
 number = loadAnimation("assets/Number00.png","assets/Number07.png");
 
+wiggle = createSprite(100,200);
+wiggle.addAnimation("1sthalf", "assets/Wiggle00.png","assets/Wiggle02.png")
+wiggle.addAnimation("2ndhalf", "assets/Wiggle00.png","assets/Wiggle05.png")
+var anim = wiggle.addAnimation("3rdquarter","assets/Wiggle00.png","assets/Wiggle08.png")
+
+wiggle.setCollider("circle",0,0,70);
+
+wiggle.onMouseOver = function(){
+  this.changeAnimation("2ndhalf")
+}
+
+wiggle.onMouseOut = function(){
+  this.changeAnimation("1sthalf")
+}
+
+wiggle.onMousePressed = function() {
+  this.changeAnimation("3rdquarter");
+  this.animation.goToFrame(this.animation.getLastFrame());
+  }
+  
+  wiggle.onMouseReleased = function() {
+  this.changeAnimation("3rdquarter");
+  this.animation.goToFrame(0);
+  }  
 }
 
 function setup() {
   createCanvas(600,600);
+  rbw = createSprite(400,400);
+  rbw.addAnimation("default",rainbow);
+  spr = createSprite(30, 30);
+  spr.addAnimation("default", number);
 }
 
 function draw() {
-  background(250,250,250);
-  animation(number,200,200);
+  background(255,255,255);
+  spr.position.x = mouseX + 80;
+  spr.position.y = mouseY +30;
+  if (mouseIsPressed) {
+    spr.rotation -= 2;
+  }
+  drawSprites();
 }
