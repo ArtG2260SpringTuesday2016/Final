07869d81b8231295316d352fc9e452b6df1b40ec
MistaFish31 <zhang.ma@husky.neu.edu>
2016-04-27 02:06:06 -0400

    init commit
    tetrads and movement finished
    
    -need to find way to stop the fall without breaking the loop
    -collisions
    -turning
    -constraints

--- readme.md
+++ readme.md
@@ -1,9 +1,21 @@
-Edit this with your description of your final project.
-
 Your final project must:
 - Be organized in an object oriented way meaning that you utilize constructor functions as well as organizing your functions.
 - Two objects made from classes you define must interact with one another
 - Correct use of loops (no prevalent similarly repeated operations)
 
-
 Due on April 29, 2016 **no late work will be accepted.**
+
+___________________________________
+
+For my final project, I will build Tetris in p5.js.
+This requires:
+
+-The 7 tetrad shapes (O, T, L, J, Z, S, I) which I will make using constructor functions
+-An array of objects which store the shapes
+-Keypress functions (so that the shapes can turn, move faster, or drop when the arrow keys or space bar is pressed)
+
+-collision detection
+-an overarching array to store the coordinates of the pieces that have collided
+-randomly chosed tetrad shapes which appear at the top of the screen after collision of last piece
+
+
---
+++ sketch.js
@@ -0,0 +1,143 @@
+// GLOBAL VARIABLES
+
+var tetrad=[];
+var w = 30;
+var xpos = 120;
+var ypos = 0;
+
+
+// UNIT FUNCTION
+
+function unit (x,y){
+  strokeWeight(1.2)
+  stroke('black');
+  rect(x,y,30,30);
+}
+
+// BLOCK FUNCTIONS
+
+function block(x,y,type){
+
+  if(type=="O"){
+    fill('yellow');
+    unit(x,y)
+    unit(x+w,y)
+    unit(x,y+w)
+    unit(x+w,y+w)
+  }
+  
+  else if(type=="T"){
+    fill('purple');
+    unit(x,y)
+    unit(x+w,y)
+    unit(x-w,y)
+    unit(x,y-w)
+  }
+  
+  else if(type=="J"){
+    fill('blue');
+    unit(x,y)
+    unit(x,y-w)
+    unit(x,y+w)
+    unit(x+w,y-w)
+  }
+  
+  else if(type=="L"){
+    fill('orange');
+    unit(x,y)
+    unit(x,y-w)
+    unit(x,y+w)
+    unit(x+w,y+w)
+  }
+  
+  else if(type=="Z"){
+    fill('red');
+    unit(x,y)
+    unit(x+w,y)
+    unit(x,y-w)
+    unit(x-w,y-w)
+  }
+  
+  else if(type=="S"){
+    fill('green');
+    unit(x,y)
+    unit(x+w,y)
+    unit(x,y+w)
+    unit(x-w,y+w)
+  }
+  
+  else if(type=="I"){
+    fill('#89f');
+    unit(x,y)
+    unit(x-w,y)
+    unit(x+w,y)
+    unit(x+w+w,y)
+  }
+}
+
+// TETRAD CLASS
+
+function Tetrad(x,y,type){
+    this.x=x;
+    this.y=y;
+    
+    var letters = [ "O", "T", "J", "L", "Z", "S", "I"];
+    var index = floor(random(letters.length));
+        
+    this.display=function(){
+        block(this.x,this.y, letters[index]);
+    }
+    
+    this.move=function(){
+        this.x+=0;
+        this.y+=1;
+        rockBottom();
+    }
+    
+    this.render=function(){
+      this.move();
+      this.display();
+    }
+}
+
+// KEY PRESSES
+
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+    tetrad[tetrad.length-1].x+=w;
+  } else if (keyCode === LEFT_ARROW) {
+    tetrad[tetrad.length-1].x-=w;
+  } else if (keyCode === DOWN_ARROW) {
+    tetrad[tetrad.length-1].y+=w;
+    return false;
+  } else if (keyCode === 32) {
+    tetrad[tetrad.length-1].y=height-30;
+    noLoop();
+  }
+}
+
+// STOP AT BOTTOM
+
+function rockBottom(){
+  if(tetrad[tetrad.length-1].y===height-30){
+    noLoop();
+  }
+}
+
+// SETUP + DRAW FUNCTION
+
+function setup(){
+    createCanvas(300,660);
+}
+
+function mousePressed(){
+  var temp = new Tetrad(xpos,ypos);
+  tetrad.push(temp);
+}
+
+function draw() {
+    background(20,20,20);
+    for (var i=0; i<tetrad.length; i++){
+      tetrad[i].render();
+    }
+}
