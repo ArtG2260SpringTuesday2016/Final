a177e9a6110c5cafa4527e6b5f3f82f50a022de7
MistaFish31 <zhang.ma@husky.neu.edu>
2016-04-27 14:35:16 -0400

    lower constraint works

--- sketch.js
+++ sketch.js
@@ -79,26 +79,24 @@ function block(x,y,type){
 // TETRAD CLASS
 
 function Tetrad(x,y,type){
-    this.x=x;
-    this.y=y;
-    
-    var letters = [ "O", "T", "J", "L", "Z", "S", "I"];
-    var index = floor(random(letters.length));
-        
-    this.display=function(){
-        block(this.x,this.y, letters[index]);
-    }
-    
-    this.move=function(){
-      if (tetrad[tetrad.length-1].y<height-w){
-        this.y+=1;
-      }
-    }
-    
-    this.render=function(){
-      this.move();
-      this.display();
-    }
+  this.x=x;
+  this.y=y;
+  
+  var letters = [ "O", "T", "J", "L", "Z", "S", "I"];
+  var index = floor(random(letters.length));
+      
+  this.display=function(){
+      block(constrain(this.x,0,300),constrain(this.y,0,630), letters[index]);
+  }
+  
+  this.move=function(){
+    this.y+=1;
+  }
+  
+  this.render=function(){
+    this.move();
+    this.display();
+  }
 }
 
 // GENERATE TETRAD
@@ -111,25 +109,17 @@ function mousePressed(){
 // KEY PRESSES
 
 function keyPressed() {
-  if (keyCode === RIGHT_ARROW) {
+  if (keyCode === RIGHT_ARROW && tetrad[tetrad.length-1].x<width ) {
     tetrad[tetrad.length-1].x+=w;
-  } else if (keyCode === LEFT_ARROW) {
+  } else if (keyCode === LEFT_ARROW && tetrad[tetrad.length-1].x>0) {
     tetrad[tetrad.length-1].x-=w;
-  } else if (keyCode === DOWN_ARROW) {
+  } else if (keyCode === DOWN_ARROW && tetrad[tetrad.length-1].y<height ) {
     tetrad[tetrad.length-1].y+=w;
     return false;
   } else if (keyCode === 32) {
     tetrad[tetrad.length-1].y=height-30;
   }
 }
-
-// STOP AT BOTTOM
-
-// function rockBottom(){
-//   if(tetrad[tetrad.length-1].y===height-30){
-//     noLoop();
-//   }
-// }
 
 // SETUP + DRAW FUNCTION
 
