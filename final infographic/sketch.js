3ce6b6f89d08d9873bcfded2fe200c17b50dbafa
Zhantao Peng <peng.z@husky.neu.edu>
2016-04-29 14:53:42 -0400

    final
    Finished. user should be able to see three data by pressing the mouse or
    moving the mouse. Added some text description into the final version as
    well.

--- final infographic/sketch.js
+++ final infographic/sketch.js
@@ -1,44 +1,43 @@
 
-var Area = function(areaData){
+var AC = function(dataset){
   var areaRect = rect
-  for (var i = 0; i < areaData.length; i++){
-    this.AbarHeight = areaData[i].area / 150;
-    this.AbarWidth = 15;
-    this.Aspacing = this.AbarWidth * 2
-    this.AbarX = this.Aspacing * i
-    this.AbarY = 1700
-    areaRect(this.AbarX, this.AbarY, this.AbarWidth, -this.AbarHeight)
+  var crimeRect = rect
+  for (var i = 0; i < dataset.length; i++){
+    this.AbarHeight = dataset[i].area / 150;
+    this.CbarHeight = dataset[i].crimeRate /150;
+    this.barWidth = 15;
+    this.spacing = this.barWidth * 2
+    this.barX = this.spacing * i
+    this.barY = 1700
+    if(mouseIsPressed){
+    fill(255,255,51)
+    crimeRect(this.barX, this.barY,this.barWidth, -this.CbarHeight)
+  }else{
+    fill(102,178,255)
+    areaRect(this.barX, this.barY, this.barWidth, -this.AbarHeight)
   }
 }
-
-var Crime = function(crimeData){
-  var crimeRect = rect;
-  for (var i = 0; i < crimeData.length; i++){
-    this.CbarHeight = crimeData[i].crimeRate / 150;
-    this.CbarWidth = 15;
-    this.Cspacing = this.CbarWidth * 2
-    this.CbarX = this.Cspacing * i
-    this.CbarY = 1700
-    crimeRect(this.CbarX, this.CbarY, this.CbarWidth, -this.CbarHeight)
-  }
 }
 
 
-var number = function(nameData){
-  var number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50];
+
+var num = function(nameData){
+  var number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
   for (var i = 0; i < number.length; i++){
   this.content = number[i];
   this.bottomPosX = i * 30 
   this.bottomPosY = 1725;
   this.sidePosX = 1250
-  this.sidePosY = i * 20
+  this.sideiniY = i * 20 
+  this.sideAdjuest = 50
+  this.sidePosY = this.sideiniY + this.sideAdjuest
     this.nameContent = nameData[i].state
     this.marginNwT = 20
-    this.posX = this.sidePosX + this.marginNwT
-    this.posY = i *20
+    this.posX = this.sidePosX + this.marginNwT 
+    this.posY = this.sidePosY
   fill(255)
   text(this.content, this.bottomPosX,this.bottomPosY)
-  text(this.content,this.sidePosX,this.sidePosY)
+  text(this.content,this.sidePosX, this.sidePosY)
   text(this.nameContent, this.posX, this.posY)
   }
   }
@@ -55,20 +54,30 @@ for(var i = 0; i < popData.length; i++){
 }
 }
 
+var legend = function(){
+  this.textPosX = 800
+  this.textPosY = 100
+  this.recPosX = this.textPosX - 50
+  this.rectPosY = this.textPosY
+  textSize(50)
+  if(mouseIsPressed){
+  text("Crime Rate", this.textPosX, this.textPosY)
+}else{
+  fill(102,178,255)
+  text("Area Size", this.textPosX, this.textPosY)
+}
+}
 
 
 function setup(){
-  createCanvas(1500,4000)
+  createCanvas(1500,2000)
+    
 }
 
 function draw(){
   background(0)
+  num(stateArea)
   ranking(population)
-  number(stateArea)
-  if(mouseIsPressed){
-    Crime(crimeRates)
-  
-  }else{
-    Area(stateArea)
-}
+  AC(AreaCrime)
+  legend()
 }
