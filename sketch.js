089924be0793e55823506fa3799838ee1f4644d8
FlyingPig13 <anotherhale.m@gmail.com>
2016-04-29 02:46:43 -0400

    Mostly Working
    A few more additions and then i'll spend the final day converting it
    into OOD code

--- sketch.js
+++ sketch.js
@@ -24,31 +24,32 @@
 //For the purposes of this program only 
 //Site_Energy_Use and Year_Built will be used
 //in order to compare amount of energy used to the age of the building
-var getHeight = "Hover over each rectangle to show the energy usage for that year";
-var getYear = "";
+var getData = "Hover over each rectangle to show the energy usage and year.";
 
-var Property = function (xposn, yposn, sizeY){
- this.y = yposn;
- this.width = width / 20;
- this.height = sizeY;
- 
- this.renderProperty = function(x) {
-  fill("Black");
-  rect(x, this.y, this.width, this.height)
- }
- this.showSize = function(x) {
-  if (collidePointRect(mouseX, mouseY, x, height - this.height)) {
-   getHeight = this.height;
+
+var Property = function(xposn, yposn, sizeY) {
+  this.y = yposn;
+  this.width = width / 20;
+  this.height = sizeY;
+
+  this.renderProperty = function(x) {
+   fill("Black");
+   rect(x, this.y, this.width, this.height)
   }
- }
- this.render = function (iterator) {
-  this.RenderProperty(iterator)
-  this.showSize(iterator)
- }
- 
+  this.showSize = function(x) {
+   if (collidePointRect(mouseX, mouseY, x, height - this.height)) {
+    getHeight = this.height;
+   }
+  }
+  this.render = function(iterator) {
+   this.RenderProperty(iterator)
+   this.showSize(iterator)
+  }
 
-}
-var currentYear = 2016;
+
+ }
+ //I know the year isn't 2014 but that is when the data ends
+var currentYear = 2014;
 var age = 50;
 var oldYear = currentYear - age;
 //These variables will hold the information 
@@ -62,45 +63,73 @@ var New = [];
 // New (equal or less than 50 years old)
 
 var years = function(ar) {
-  for (i = 0; i < ar.length; i++) {
-    if (ar[i].Year_Built < oldYear) {
-      append(Old, ar[i])
-    } else {
-      append(New, ar[i])
-    }
+ for (var i = 0; i < ar.length; i++) {
+  if (ar[i].Year_Built < oldYear) {
+   append(Old, ar[i])
+  } else {
+   append(New, ar[i])
   }
+ }
 }
 
 var sum = function(ar) {
-  var total = 0;
-  for (i = 0; i < ar.length; i++) {
-    total += ar[i];
-  }
-  return total;
+ var total = 0;
+ for (var i = 0; i < ar.length; i++) {
+  total += ar[i];
+ }
+ return total;
 }
 
 var convert = function(ar) {
-    var newAr = [];
-    for (i = 0; i < ar.length; i++) {
-      if (ar[i].Site_Energy_Use != "Not Available" && ar[i].Year_Built !== null) {
-        append(newAr, ar[i])
-      }
-    }
-    return newAr;
+ var newAr = [];
+ for (var i = 0; i < ar.length; i++) {
+  if (ar[i].Site_Energy_Use != "Not Available" && ar[i].Year_Built !== null) {
+   append(newAr, ar[i])
   }
-  
-  var getEnergy = function(ar){
+ }
+ return newAr;
+}
+
+var getEnergy = function(ar) {
+ var newAr = [];
+ for (var i = 0; i < ar.length; i++) {
+  append(newAr, Number(ar[i].Site_Energy_Use.replace(/[*]| |,/g, "")))
+ }
+ return newAr;
+}
+
+var getYear = function(ar) {
+ var newAr = [];
+ for (i = 0; i < ar.length; i++) {
+  append(newAr, ar[i].Year_Built)
+ }
+ return newAr;
+}
+
+var sortYear = function(a, b) {
+ return a.Year_Built - b.Year_Built;
+}
+
+//Due to some problems on my end I am splitting my initial
+//data into two arrays; one holding Year_Built and one holding
+//Site_Energy_Use which is why this function needs two arrays
+var avgYear = function(arYear, arEnergy) {
+ var newAr = [];
+ var arAvg = [];
+ for (var i = 0; i < (arYear.length - 1); i++) {
+  console.log(i);
+  append(newAr, arEnergy[i])
+  if (arYear[i] !== arYear[i+1]) {
+   append(arAvg, [arYear[i], round(sum(newAr) / newAr.length)])
    newAr = [];
-   for (i = 0; i < ar.length; i++) {
-    append(newAr, Number(ar[i].Site_Energy_Use.replace(/[*]| |,/g, "")))
-   }
-   return newAr;
   }
-  
-  var sortYear = function(a, b) {
-   return a.Year_Built - b.Year_Built;
-   }
-  
+ }
+ append(newAr, arEnergy[arYear.length - 1])
+ append(arAvg, [arYear[i], round(sum(newAr) / newAr.length)])
+ console.log(arAvg.length)
+ return arAvg;
+}
+
 
 
 
@@ -109,78 +138,88 @@ var convert = function(ar) {
 //omitting any that appear as "Not Available"
 
 function setup() {
-  createCanvas(1200, 750);
-  background(200, 255, 255);
+ createCanvas(1200, 750);
+ background(200, 255, 255);
 
-  
  years(data);
  Old = convert(Old);
  New = convert(New);
+
  Old.sort(sortYear);
  New.sort(sortYear);
- console.log(Old);
 
-  var OldAvg = sum(getEnergy(Old)) / Old.length;
-  var NewAvg = sum(getEnergy(New)) / New.length;
-  
-  stroke(30, 0 ,30);
-  var resize = 200000
-  textSize(25);
-  fill(200, 0, 150);
-  strokeWeight(3);
-  
-  
-  var makeEnergy = function(avg, size, xposn, yposn) {
-   rect(width/xposn, yposn - (avg / size), width / 5, avg / size)
-  }
-  
-  var oldenergy = makeEnergy(OldAvg, resize, 1.85, 625);
-  oldenergy;
-  
-  fill(100, 30, 250)
-  strokeWeight(2);
-  var newenergy = makeEnergy(NewAvg, resize, 1.3, 625);
-  newenergy;
+ OldYears = getYear(Old);
+ OldEnergy = getEnergy(Old);
+ NewYears = getYear(New);
+ NewEnergy = getEnergy(New);
+ console.log(OldYears);
+ console.log(OldEnergy);
+ //console.log(avgYear(OldYears, OldEnergy));
+ 
 
-  fill("Blue");
-  stroke("Black")
-  strokeWeight(0)
-  text(round(OldAvg), width / 1.7, height * .9);
-  text(round(NewAvg), width / 1.23, height * .9);
-  
-  textSize(50);
-  fill("White")
-  strokeWeight(5);
-  stroke("black");
-  text("Average Energy Use", width / 1.75, height / 8);
-  
-  fill("Black")
-  rect(0, height * .835, width, 0);
-  rect(0, height * .15, width, 0);
-  rect(width / 2, height * .15, 0, height * .68);
-  rect(0, height / 2, width/2, 0);
-  
- 
-   //Compare old:energy to new:energy; two functions; one with all old energies,
-  //one with all new energies
- 
+
+ var OldAvg = sum(getEnergy(Old)) / Old.length;
+ var NewAvg = sum(getEnergy(New)) / New.length;
+
+ stroke(30, 0, 30);
+ var resize = 200000
+ textSize(25);
+ fill(200, 0, 150);
+ strokeWeight(3);
+
+
+ var makeEnergy = function(avg, size, xposn, yposn) {
+  rect(width / xposn, yposn - (avg / size), width / 5, avg / size)
+ }
+
+ var oldenergy = makeEnergy(OldAvg, resize, 1.85, 625);
+ oldenergy;
+
+ fill(100, 30, 250)
+ strokeWeight(2);
+ var newenergy = makeEnergy(NewAvg, resize, 1.3, 625);
+ newenergy;
+
+ fill("Blue");
+ stroke("Black")
+ strokeWeight(0)
+ text(round(OldAvg), width / 1.7, height * .9);
+ text(round(NewAvg), width / 1.23, height * .9);
+
+ textSize(50);
+ fill("White")
+ strokeWeight(5);
+ stroke("black");
+ text("Average Energy Use", width / 1.75, height / 8);
+
+ fill("Black")
+ rect(0, height * .835, width, 0);
+ rect(0, height * .15, width, 0);
+ rect(width / 2, height * .15, 0, height * .68);
+ rect(0, height / 2, width / 2, 0);
+
+
+ //Compare old:energy to new:energy; two functions; one with all old energies,
+ //one with all new energies
+
 }
 
 function draw() {
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
  fill("Purple")
- textSize(35)
- text()
+ strokeWeight(0)
+ textSize(20)
+ text(getData, 0, height / 8);
 }
